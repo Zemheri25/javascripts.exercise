51d7ec1e50d239d927816032c57e17c05b50c769
Zemheri25 <omerzemheri14@gmail.com>
2021-12-20 17:48:18 +0300

    new exercise

---
+++ first.js
@@ -0,0 +1,679 @@
+// This is my first commend in the js.
+// var number = 5;
+// var Myname = "ömer";
+// let ourName = "freecodecamp"
+
+// var Myname = "ömer";
+// console.log(Myname);
+
+// var a;
+// var b = 2;
+// console.log(b)
+// console.log(a)
+
+// a = 7;
+// b = a;
+
+// console.log(a)
+// console.log(b)
+
+// var a = 9;
+// var b = 10;
+// var c = "I am a"
+
+// a += 1;
+// b += 2
+// c += "string"
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var sum = 10 + 10;
+// console.log(sum)
+
+// var product = 8 * 0;
+// console.log(product)
+
+// var divisible = 10 / 2;
+// console.log(divisible)
+
+// var Myvar = 10;
+// Myvar++;
+// console.log(Myvar)
+
+// var Myvar = 10;
+// Myvar--;
+// console.log(Myvar)
+
+
+// var ourDecimal = 5.7;
+// var myDecimal = 0.0009;
+
+// console.log(ourDecimal)
+// console.log(myDecimal)
+
+// var product = 2.0 * 2.5;
+// console.log(product)
+
+// var my = 4.0 / 2.2;
+// console.log(my)
+
+// var my = 18 % 2;
+// console.log(my)
+
+// var my = 10;
+// my -= 1;
+// console.log(my)
+// my--;
+// console.log(my)
+
+// var my = 5;
+// my *= 2;
+// console.log(my)
+
+// var firstname = "ömer"
+// var lastname = "zemheri"
+
+// fullname = firstname + " " + lastname
+// console.log(fullname)
+
+// var mystr = 'My name is ömer "and dagsd" hdgjf'
+// console.log(mystr)
+
+// var mystr = "hello\n\tMy name is Ömer\n\t\tI am from Trukey";
+// console.log(mystr)
+
+// var mystr = "my name is ömer  \\ muah ";
+// console.log(mystr)
+
+
+// var mystr = "I come first.";
+// mystr += " I come second";
+// console.log(mystr)
+
+// var name = "ömer."
+// var mysentence = "Hello.My name is " + name + "I am from Turkey";
+// console.log(mysentence)
+
+// var name = "ömer";
+// var surname = "zemheri";
+// surname += name;
+// console.log(surname)
+
+
+// var firstname = "ömer";
+// firstnamelenght = firstname.length;
+// console.log(firstnamelenght)
+
+// var firstname = "ömer"
+// firstcharacter = firstname[1];
+// console.log(firstcharacter)
+
+
+// var firstname = "ömer";
+// last = firstname[firstname.length - 1];
+
+
+// var lastname = "zemheri";
+// last = lastname[lastname.length-1];
+// console.log(last)
+
+// var surname = "zemheri";
+// last2 = surname[surname.length-2];
+// console.log(last2);
+
+// function wordBlanks(myname, country, age){
+//     var result = "";
+//     result += "My name is " + myname + ".I am from " + country + ".And I am " + age + " years old."
+//     return result;
+// }
+// console.log(wordBlanks("ömer", "Turkey", "27"))
+
+// var ourArray = [2, 3, "ömer"];
+// first = ourArray[0];
+// console.log(first)
+
+// last = ourArray[ourArray.length-1];
+// console.log(last)
+
+// var myarray = [[true, 2, 3, 4], [false]];
+// first = myarray[0];
+// console.log(first)
+// console.log(myarray[1])
+
+// var mylist = [1, 2, 3, 4];
+// mylist[0] = 7;
+// console.log(mylist);
+
+// var myarray = [[1,2,3], [4,5,6], [7,8,9]];
+// var data = myarray[0][0] 
+// console.log(data);
+ 
+// var mylistt = [1, 2, 3, 4];
+// mylistt.push([5, 6]);
+// console.log(mylistt);
+
+// var mylist = [1, 2, 3, 4];
+// a = mylist.pop();
+// console.log(mylist);
+// mylist.shift();
+// console.log(mylist);
+
+// var mylist = [["ömer", 27], ["bahar", 27], ["şeyda", 25]]
+// mylist.shift();
+// mylist.unshift(["fatma", 40]);
+// console.log(mylist); 
+
+// var mylist = [["cereal", 3], ["milk", 2], ["bananas", 3], ["juice", 2], ["eggs", 10]];
+// console.log(mylist)
+
+// function saysomething() {
+//     console.log("Hello World");
+// }
+
+// saysomething();
+
+
+
+// function çıkarma(a,b){
+//     console.log(a-b);
+// };
+
+// (çıkarma(3, 5));
+
+// function toplama(a,b) {
+//     console.log(a+b);
+// };
+
+
+// var myglobal = 10;
+
+// function fun1() {
+//     oopsGlobal = 5;
+// }
+
+// function fun2() {
+//     var output = ""
+//     if (typeof myglobal != "undefined") {
+//         output += "myglobal : " + myglobal;
+//     }
+//    if (typeof oopsGlobal != "undefined") {
+//     output += "oopsGlobal: " + oopsGlobal;
+//     }
+//     console.log(output);
+
+// }
+
+// fun1();
+// fun2();
+
+// var outwear = "Tshirt";
+// function mywear() {
+//     outwear = "Mont"
+//     return outwear
+// };
+// console.log(mywear())
+
+
+// var mynum = 5
+
+// function toplama() {
+//     return mynum += 5;
+// };
+
+// console.log(toplama())
+
+ 
+// var changed = 0;
+
+// function change(num) {
+//     return (num + 10) / 3
+// }
+
+// changed = change(10)
+// console.log(changed)
+
+// var testarray = [1, 2, 3, 4, 5];
+
+// function pushing(list1, item) {
+//     list1.push(item);
+//     return list1
+
+// };
+
+
+// console.log("Before : " + JSON.stringify(testarray));
+// (pushing(testarray, 6));
+// console.log("After : " + JSON.stringify(testarray));
+
+// var a= "The last value of my list is : " + testarray[testarray.length-1]
+// console.log(a)
+
+// function myvalue(values) {
+//     if (values) {
+//         return "Yes that is True"
+//     }
+
+//     return "No that is not True"
+// };
+
+
+// console.log(myvalue(false));
+
+// var myage = 27;
+// function isitmyage(age) {
+//     if (age == 27) {
+//         return "Yes thats my age"
+
+//     }
+
+//     return "That is not myage"
+// };
+
+
+
+// console.log(isitmyage(myage))
+ 
+// function büyükmü(num) {
+//     if (num >= 100) {
+//         return "ıt is greater than 100 or equal 100"
+//     };
+
+//     if (num >= 80) {
+//         return "ıt is greater than 80 or equal"
+//     };
+
+//     if (num >= 30) {
+//         return "ıt is greater than 30 or equal"
+//     }
+// };
+
+// console.log(büyükmü(79))
+
+// function ortada(num) {
+//     if (num > 80){
+//         if(num <100){
+//             return "Yes"
+//         }
+//     }
+//    return "No"
+// };
+
+// console.log(ortada(100))
+
+
+// var myage = 31;
+// function ortada(age) {
+//     if (age < 30 && age > 20) {
+//         return true
+//     }
+//    return false
+// };
+// console.log(ortada(myage));
+
+
+// var myage= 31
+// var myname = "ömer"
+
+// function dogrumu(age, firstname) {
+//     if(age < 30 || firstname == "ahmet") {
+//         return "One of tehm true"
+//     }
+//    return "None of them true"
+// };
+
+// console.log(dogrumu(myage, myname))
+
+
+// var mynumber = 15;
+
+// function nerde(num) {
+//     if (num < 10) {
+//         return "Lessthan 10"
+//     }
+//      else if (num > 20){
+//          return "Greater than 20"
+//      }   
+//     else {
+//         return "between 10 and 20"
+//     }
+// };
+
+// console.log(nerde(mynumber), "Your number = ", mynumber)
+
+
+// function nerde(num) {
+//     if (num < 5) {
+//         return "Tiny"
+//     }
+//     else if (num < 10) {
+//         return "Small"
+//     }
+
+//     else if (num <15) {
+//         return "Medium"
+//     }
+
+//     else if (num < 20) {
+//         return "Large"
+//     }
+
+//     else {
+//         return "Huge"
+//     }
+// }
+
+
+// console.log(nerde(3))
+
+// var myarray = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double bogey", "Go home!"]
+
+// function golfScore(par, strokes) {
+//     if (strokes == 1) {
+//         return myarray[0]
+//     }
+//     else if (strokes <= par -2) {
+//         return myarray[1]
+//     }
+//     else if (strokes == par -1) {
+//         return myarray[2]
+//     }
+//     else if (strokes == par) {
+//         return myarray[3]
+//     }
+//     else if (strokes == par +1) {
+//         return myarray[4]
+//     }
+//     else if (strokes == par +2) {
+//         return myarray[5]
+//     }
+//     else if (strokes >= par +3) {
+//         return myarray[myarray.length -1] 
+        
+//     }
+//     else {
+//         return "Change me"
+//     }
+// }
+
+// console.log(golfScore(6, 15))
+
+// function check(val) {
+//     var answer = ""
+//     switch(val) {
+//         case 1:
+//             answer = "alpha";
+//             break;
+//         case 2:
+//             answer = "beta";
+//             break;
+//         case 3:
+//             answer = "teta";
+//             break;
+//         case 4:
+//             answer = "delta";
+//             break;    
+    
+//         }
+//     return answer
+// }
+
+// console.log(check(4))
+
+
+// function ömer(val) {
+//     answer = ""
+//     switch(val) {
+//         case "a":
+//             answer = "apple"
+//             break;
+//         case "b":
+//             answer = "banana"
+//             break;
+//         case "d":
+//             answer = "danny"
+//             break;
+//         case "e":
+//             answer = "egg"
+//             break;
+//         default:
+//             answer = "ömer zemheri"
+//             break; 
+//     }
+//     return answer 
+// }
+
+// console.log(ömer(3))
+
+
+
+// function something(val) {
+//     answer = ""
+//     switch(val) {
+//         case 1:
+//         case 2:
+//         case 3:
+//         case 4:
+//             answer = "hi"
+//             break;
+//         case 5:
+//         case 6:
+//         case 7:
+//         case 8:
+//             answer = "mid"
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//         case 12:
+//             answer = "high"
+//             break;
+//         default:
+//             answer = "ömer zemheri always"
+
+//     }
+//     return answer
+// }
+
+// console.log(something(12))
+
+
+
+// var count = 0;
+
+// function cc(card) {
+//     switch(card) {
+//         case 1:
+//         case 2:
+//         case 3:
+//         case 4:
+//         case 5:
+//         case 6:
+//             count++;
+//             break;
+//         case 10:
+//         case "J":
+//         case "Q":
+//         case "K":
+//         case "A":
+//             count--;
+//             break;
+
+        
+//     }
+//     var holdbet = "Hold"
+//     if (count > 0) {
+//         holdbet = "Bet"
+//     }
+    
+//     return count + " " + holdbet
+// }
+
+// cc("J"); cc("A"); cc(7); cc("K"); 
+// console.log(cc(4))
+
+
+// var ourdog = {
+//     "name is" : "ömer",
+//     "legs" : 4,
+//     "tails" : 1,
+//     "friends" : ["everything"]
+// };
+
+// var myname = ourdog["name is"];
+
+// console.log(myname)
+
+// var test = {
+//     12:"nomath",
+//     16:"montana",
+//     19:"unitas"
+// };
+
+// var playerNumber = 16;
+// var player = test[playerNumber]
+// console.log(player)
+
+ 
+// var mydict = {
+//     "name is" : "ömer",
+//     "surname" : "zemheri",
+//     "age" : 27
+// };
+
+// mydict["name is"] = "Bahar"
+// mydict["from"] = "Erzurum"
+// delete mydict["surname"]
+
+// console.log(mydict);
+
+
+// function phonetic(val) {
+//     var result = "";
+//     var lookup = {
+//         "alpha": "Adams",
+//         "bravo": "Boston",
+//         "charlie": "Chicago",
+//         "delta": "Denver",
+//         "echo": "Easy",
+//         "foxtrot": "frank"
+//     };
+//     result = lookup[val];
+
+//     return result
+// }
+
+
+// console.log(phonetic("bravo"))
+
+// var mydict = {
+//     "gift": "pony",
+//     "pet" : "kitten",
+//     "bed" : "sleigh"
+
+// };
+
+// function checkobj(checkpro){
+//     if (mydict.hasOwnProperty(checkpro)) {
+//         return mydict[checkpro];
+//     }else{
+//         return "Not Found"
+//     }
+// }
+
+// console.log(checkobj("pet"))
+
+// var myarray = [
+//     {"name":"ömer",
+//      "surname": "zemheri",
+//     "from": "Trukey",
+//     "age": 27,
+//     "hobbies": ["playing football", "writing code", "watching tv"],
+//      }, 1, 2, 3,{
+//          "name"  :"Bahar",
+//          "surname": "Zemheri",
+//          "age" : 27,
+//          "from": "Turkey",
+//          "hobbies": ["sleeping", "watching tv", "testing code"]
+//      }
+// ]
+
+// console.log(myarray[1])
+
+
+// var mylist = [];
+
+// for (var i = 1; i < 10; i+= 2) {
+//     mylist.push(i)
+// } 
+    
+// console.log(mylist)
+
+
+// var mylist = [];
+// for (var i = 10 ; i > 0; i--) {
+//     mylist.push(i);
+// }
+
+// console.log(mylist)
+
+// var mylist = [8, 9, 12, 13, 14];
+// var newlist = 0;
+
+// for (var i = 0; i < mylist.length; i++) {
+//     newlist += mylist[i];
+// }
+
+// console.log(newlist)
+
+
+// function çarpma(list1) {
+//     var count = 1;
+//     for (var i = 0; i < list1.length; i++) {
+//         for (var j = 0; j < list1[i].length; j++) {
+//             count *= list1[i][j]
+//         }
+//     }
+//    return count
+// }
+
+// console.log(çarpma([[1,2], [3, 4], [5, 6, 7]]))
+
+
+var mydict = [
+    {"firstname" : "ömer",
+     "lastname" : "zemheri",
+     "from" : "Trukey",
+     "age" : 27},
+
+     {"firstname": "Bahar",
+     "lastname" : "Zemheri",
+     "from" : "Trukey" , 
+     "age" : 28},
+
+     {"firstname" : "şeyda",
+     "lastname" : "yılmaz", 
+     "from": "kırıkkale",
+     "age": 24},
+
+     {"firstname" : "nafiz",
+     "lastname" : "zemheri", 
+     "from": "Turkey",
+     "age": 50},
+
+     {"firstname" : "fatma",
+     "lastname" : "Zemheri",
+     "from" : "Turkey",
+     "age": 44}
+]
+
+function arama(first, prop) {
+    for (var i = 0; i < mydict.length; i++) {
+        if (first == mydict[i]["firstname"]) {
+            return mydict[i][prop]
+        }
+    }return "No one here."
+}
+
+console.log(arama("nafiz", "age"))
